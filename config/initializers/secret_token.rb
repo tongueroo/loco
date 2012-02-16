6ea5b9a6c6ae7f33bf0127e79019dac30ead4400
Tung Nguyen <tongueroo@gmail.com>
2012-02-16 00:48:34 -0800

    init commit, rails new loco -d mysql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Loco::Application.config.secret_token = '01cc5b88e011019f741f16eede2ba131e2760999e1dc764bf3f698188d1b31e3b6c9495a4ef72de27957670ab5070d9e4646e776ec3c2469caf4ad5c28d2ef91'
